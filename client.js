5c8316a67e460e7d1d876db611af2278d779417c
Cole R Lawrence <cole@zombiehippie.com>
2014-10-11 15:11:12 -0500

    Improvements 2.0

--- client.js
+++ client.js
@@ -1,30 +1,50 @@
-var wsUri="ws://chat.fy206.info:6078"
+var wsUri="ws://localhost:6078"
 var wss=new window.WebSocket(wsUri)
 var log=document.getElementById("messagelog")
+var onlineEl=document.getElementById("online")
+var online=[]
 wss.onopen = function () {
-  var name=prompt("What is your name?").split(":")[0]
+  var name = prompt("What is your unique name?").split(":")[0]
+  wss.send(name+":+::"+Date.now())
   wss.onmessage = function (event) {
     msg=event.data.split(":")
-    switch(msg[0]){
+
+    type = msg[1]
+    sender = msg[0]
+    text = msg[2]
+    time = ""
+    try {
+      date = new Date(parseInt(msg[3]))
+      time = date.toLocaleString()
+    } catch(err){}
+
+    switch(type) {
       case "m":
-        tr=document.createElement("TR")
-        time = ""
-        try {
-          date = new Date(parseInt(msg[3]))
-          time = date.toLocaleString()
-        } catch(err){}
-        tr.innerHTML = "<td>"+time+"</td><td><strong>"+msg[1]+":</strong></td><td>"+msg[2].replace("`",":")+"</td>"
+        var tr=document.createElement("TR")
+        tr.innerHTML = "<td>"+time+"</td><td><strong>"+sender+":</strong></td><td>"+text.replace("`",":")+"</td>"
         log.appendChild(tr)
         break;
       case "s":
-        tr=document.createElement("TR")
-        tr.innerHTML = "<td><strong style='color:magenta'>Server:</strong></td><td>"+msg[1].replace("`",":")+"</td>"
+        var tr=document.createElement("TR")
+        tr.innerHTML = "<td>"+time+"</td><td><strong style='color:magenta'>Fuck Yeah 206:</strong></td><td>"+text.replace("`",":")+"</td>"
         log.appendChild(tr)
+        break;
+      case "+":
+        var li=document.createElement("LI")
+        li.innerText=text
+        li.id="user-"+text
+        onlineEl.appendChild(li)
+        break;
+      case "-":
+        var li = document.getElementById("user-"+text)
+        li != null && li.remove()
         break;
     }
   }
   window.sendMessage = function (message) {
-    wss.send(name + ":" + message.replace(":","`") + ":" + Date.now())
+    if (message.length)
+      wss.send(name+":m:" + message.replace(":","`") + ":" + Date.now())
+    return false
   }
   document.getElementById('send_btn').style.display = null
 }
---
+++ server-simple.coffee
@@ -0,0 +1,80 @@
+http = require('http')
+WebSocketServer = require('ws').Server
+wss = new WebSocketServer({port: 6078})
+fs = require 'fs'
+require './node_modules/jade-dev/jade-dev.js'
+
+clients = []
+lastsender = null
+msgsrow = 0
+
+Msg = (name, text, type="m") ->
+  [name, type, text.replace(/[:\n]/g,'`'), Date.now()].join(':')
+
+broadcast = (name, text, type="m") ->
+  data = Msg(name, text, type)
+  if type is "m"
+
+    # Prevent spamming
+    if lastsender is name
+      msgsrow++
+      if msgsrow > 4
+        # too many in a row
+        return
+    else
+      msgsrow = 0
+      lastsender = name
+
+    fs.appendFile "./messages.log", data + "\n", ((error)->error? and console.error(error))
+  for client in clients when client._chatname?
+    client.send data
+
+  console.log localDate(), data
+
+localDate = ->
+  (new Date).toLocaleString()
+
+onClientConnection = (ws) ->
+  ws.on "message", onClientMessage
+  ws.on "close", onClientClose
+  clients.push ws
+
+onClientClose = ->
+  selfIndex = clients.indexOf @
+  if selfIndex isnt -1
+    clients.splice(selfIndex, 1)[0]
+    if @_chatname
+      broadcast(null, client._chatname + " left the chatroom", "s")
+      broadcast(null, @_chatname, "-")
+
+onClientMessage = (data) ->
+  messageParts = data.split(":")
+  self = @
+
+  if messageParts.length is 4
+    m = {
+      name: messageParts[0]
+      type: messageParts[1]
+      text: messageParts[2]
+      date: messageParts[3]
+    }
+
+    switch m.type
+      when "+"
+        # Logged on
+        self._chatname = m.name
+
+        fs.readFileSync("./messages.log", "utf8").split("\n").slice(0,25).forEach self.send.bind(self)
+
+        # Update new log client's online list
+        for client in clients when client isnt self and client._chatname
+          self.send Msg(null, client._chatname, "+")
+
+        self.send Msg(null, "Welcome to the most bomb-ass chatroom!", "s")
+        broadcast(null, client._chatname + " joined the chatroom", "s")
+        broadcast(null, m.name, "+")
+
+      when "m"
+        broadcast(m.name, m.text)
+
+wss.on('connection', onClientConnection)
